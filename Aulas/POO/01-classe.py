fee761250d4c710bc2009dd745e3369b774558ab
Delson <delsonmafumo24@gmail.com>
2026-06-21 18:20:22 +0200

    continuaçao com classes Self & Init

--- Aulas/POO/01-classe.py
+++ Aulas/POO/01-classe.py
@@ -1,10 +1,37 @@
-class ControleRemoto:
-    def __init__():
-        cor = "preto "
-        altura = ""
-        profundidade = ""
-        largura = ""
+# vamos criar uma classe pra site da netflix
 
-
+class Cliente:
+    def __init__(self, nome, email, plano_assinatura):
+        self.nome = nome
+        self. email = email
+        self.lista_plano = ["basic", "premium"]
+        if plano_assinatura in self.lista_plano:
+            self.plano_assinatura = plano_assinatura
+        else:
+            raise Exception("Plano Invalido ")
         
- 
+    # funçao mudar plano
+    def mudar_plano(self, novo_plano):
+        if novo_plano in self.lista_plano:
+            self.plano_assinatura = novo_plano
+        else:
+            print("Plano Invalido")
+
+    # funçao ver filme 
+    def ver_filme(self, filme, plano_filme):
+        if self.plano_assinatura == plano_filme:
+            print(f"Ver o filme{filme}")
+        elif self.plano_assinatura == "premium":
+            print("ver filme")
+        elif self.plano_assinatura == "basic" and plano_filme == "premium":
+            print("faça upgrade dp plano para o plano premiu pra poder ver o filme")
+        else: 
+            print("Plano invelido")
+
+cliente = Cliente("Delson ", "delson@gmail.com", "premium")
+print(cliente.plano_assinatura)
+cliente.ver_filme("Harry poter", "premium")
+
+# botao de fazer upgrade
+cliente.mudar_plano("premium")
+print(cliente.plano_assinatura)
