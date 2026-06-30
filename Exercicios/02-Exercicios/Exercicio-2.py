f887581fefcadf9a4b465398266749b042957042
Delson <delsonmafumo24@gmail.com>
2026-06-30 21:25:08 +0200

    resolucao de exercicios sobre textos

---
+++ Exercicios/02-Exercicios/Exercicio-2.py
@@ -0,0 +1,15 @@
+# Exercício 2: Padronização de Dados de CRM (Setor de Vendas) Um vendedor cadastrou um cliente com os dados desorganizados no sistema: nome = " mArCoS aNtOnIo rOcHa " e email = " MARCOS.ROCHA@GMAIL.COM ". Para evitar duplicidade e erros de envio, você deve:
+# 1.	Remover os espaços extras no início e fim das duas variáveis.
+# 2.	Deixar o nome apenas com as primeiras letras de cada palavra em maiúsculo (formato de nome próprio).
+# 3.	Deixar o e-mail todo em letras minúsculas. Exiba os resultados finais no console.
+
+
+nome = " mArCoS aNtOnIo rOcHa "
+email = " MARCOS.ROCHA@GMAIL.COM "
+
+# resposta: 2 & 3
+
+nome_retificado = nome
+email_retificado = email
+print(nome.title(),  email.upper())
+
